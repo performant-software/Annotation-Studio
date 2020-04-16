18dc4a15d3df1f2d033ee2ee8c4238b00f4d467c
Tim Dunn <timdunn2@gmail.com>
2020-04-16 20:27:51 +0545

    Add ability to create anthology without banners and without errors

--- app/models/anthology.rb
+++ app/models/anthology.rb
@@ -15,23 +15,27 @@ class Anthology < ActiveRecord::Base
   attr_accessor :error_messages
 
   def check_content_type
-    if !['image/jpg', 'image/jpeg', 'image/gif','image/png'].include?(self.banner_content_type)
-      errors.add(:banner, "File '#{self.banner_file_name}' is not a valid image type")
-      unless self.error_messages
-      self.error_messages =  "File '#{self.banner_file_name}' is not a valid image type"
-      else
-        self.error_messages =  "#{self.error_messages} and File '#{self.banner_file_name}' is not a valid image type"
+    if self.banner_content_type.present?
+      if !['image/jpg', 'image/jpeg', 'image/gif','image/png'].include?(self.banner_content_type)
+        errors.add(:banner, "File '#{self.banner_file_name}' is not a valid image type")
+        unless self.error_messages
+          self.error_messages =  "File '#{self.banner_file_name}' is not a valid image type"
+        else
+          self.error_messages =  "#{self.error_messages} and File '#{self.banner_file_name}' is not a valid image type"
+        end
       end
     end
   end
 
   def check_file_size
-    if !( self.banner_file_size < 1.megabytes )
-      errors.add(:banner, "File is too big")
-      unless self.error_messages
-      self.error_messages =  "File '#{self.banner_file_name}' should be less than 1 MB"
-      else
-        self.error_messages =  "#{self.error_messages} and File '#{self.banner_file_name}' should be less than 1 MB"
+    if self.banner_file_size.present?
+      if !( self.banner_file_size < 1.megabytes )
+        errors.add(:banner, "File is too big")
+        unless self.error_messages
+          self.error_messages =  "File '#{self.banner_file_name}' should be less than 1 MB"
+        else
+          self.error_messages =  "#{self.error_messages} and File '#{self.banner_file_name}' should be less than 1 MB"
+        end
       end
     end
   end
