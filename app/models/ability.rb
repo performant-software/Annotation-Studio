d338b0e9d2cc0ed04a89b75ec03c12a15a1c2609
Tim Dunn <timdunn2@gmail.com>
2020-05-29 21:01:29 +0545

    Enable all users to access documents under their own anthologies

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -22,12 +22,14 @@ class Ability
       end
       can :create, Document
       can [:read, :update, :annotatable, :review, :publish, :archive, :preview, :post_to_cove, :export, :set_default_state, :snapshot], Document, { :user_id => user.id }
+      can [:read, :update], Anthology, { :user_id => user.id }
       can :destroy, Document, { :user_id => user.id, :published? => false }
 
     elsif user.has_role? :student
       cannot :manage, Document
       can :create, Document
       can [:read, :update], Document, { :user_id => user.id }
+      can [:read, :update], Anthology, { :user_id => user.id }
       can :destroy, Document, { :user_id => user.id, :published? => false }
       can :read, Document do |tors|
         !(user.rep_group_list & tors.rep_group_list).empty?
