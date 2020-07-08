3cf56d8dc5539b8ba20bfa530c3209ef8818f62c
Jamie Folsom <jamiefolsom@gmail.com>
2020-07-08 12:17:57 -0400

    Un-ignore application assets.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,12 +4,12 @@
 Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
-# Rails.application.config.assets.paths << Emoji.images_path
+# Rails.application.config.assets.paths += %w(app/assets/images/)
 
 Rails.application.config.assets.precompile += %w(active_admin.css active_admin.js users.css
 widget.js summernote.eot summernote.ttf summernote.woff glyphicons-halflings-regular.eot glyphicons-halflings-regular.woff2
 glyphicons-halflings-regular.woff glyphicons-halflings-regular.ttf documents.css
-catalog.css tiny_mce_popup.js groups.css annotations.css)
+catalog.css tiny_mce_popup.js groups.css annotations.css *.png)
 
 Rails.application.config.assets.enabled = true
 Rails.application.config.assets.digest = true
