87affb0b9a8641e9960a34102bc63bbe532a86b4
Jamie Folsom <jamiefolsom@gmail.com>
2020-07-08 13:02:05 -0400

    Explicitly include some pngs

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,7 +9,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w(active_admin.css active_admin.js users.css
 widget.js summernote.eot summernote.ttf summernote.woff glyphicons-halflings-regular.eot glyphicons-halflings-regular.woff2
 glyphicons-halflings-regular.woff glyphicons-halflings-regular.ttf documents.css
-catalog.css tiny_mce_popup.js groups.css annotations.css *.png)
+catalog.css tiny_mce_popup.js groups.css annotations.css cove_footer_logo.png hs-logo-trans-horiz.png logo_mit.png neh_at_logo.png)
 
 Rails.application.config.assets.enabled = true
 Rails.application.config.assets.digest = true
