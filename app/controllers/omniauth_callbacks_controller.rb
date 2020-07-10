857752317ce96e321c6373804d4870b478410ad4
Jamie Folsom <jamiefolsom@gmail.com>
2020-07-10 12:43:38 -0400

    Make OAuth provider name configurable

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -2,7 +2,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def wordpress_hosted
     @user = User.find_for_wordpress_oauth2(request.env["omniauth.auth"], current_user)
     if @user.persisted?
-      flash[:notice] = I18n.t("devise.omniauth_callbacks.success", :kind => $DOMAIN_CONFIG['site_name'])
+      flash[:notice] = I18n.t("devise.omniauth_callbacks.success", :kind => $DOMAIN_CONFIG['oauth_provider'])
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
     else
       session["devise.wordpress_oauth2_data"] = request.env["omniauth.auth"]
