ed6334f8895fef510ceb31018b3f2bc5417b2cf7
Tim Dunn <timdunn2@gmail.com>
2020-07-16 02:55:08 +0545

    Change ruby version back

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -52,7 +52,7 @@ Devise.setup do |config|
   # Tell if authentication through request.params is enabled. True by default.
   # It can be set to an array that will enable params authentication only for the
   # given strategies, for example, `config.params_authenticatable = [:database]` will
-  # enable it only for database (email  password) authentication.
+  # enable it only for database (email + password) authentication.
   # config.params_authenticatable = true
 
   # Tell if authentication through HTTP Basic Auth is enabled. False by default.
