a3e689871e7806ded3c1bed72a9671e04fb2265f
Jamie Folsom <jamiefolsom@gmail.com>
2020-08-18 10:28:16 -0400

    Adjust anthology permissions.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -22,16 +22,18 @@ class Ability
       end
       can [:read, :create], Document
       can [:read, :update, :annotatable, :review, :publish, :archive, :preview, :post_to_cove, :export, :set_default_state, :snapshot, :anthology_add], Document, { :user_id => user.id }
-      can [:read], Anthology
+      cannot :manage, Anthology
       can :manage, Anthology, { :user_id => user.id }
+      can [:read], Anthology
       can :destroy, Document, { :user_id => user.id, :published? => false }
 
     elsif user.has_role? :student
       cannot :manage, Document
       can [:read, :create], Document
       can [:read, :update, :anthology_add], Document, { :user_id => user.id }
-      can [:read], Anthology
+      cannot :manage, Anthology
       can :manage, Anthology, { :user_id => user.id }
+      can [:read], Anthology
       can :destroy, Document, { :user_id => user.id, :published? => false }
       can :read, Document do |tors|
         !(user.rep_group_list & tors.rep_group_list).empty?
