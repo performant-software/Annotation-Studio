1d0094bded92da7fe1339bdae1559930ffa9c0d8
Camden Mecklem <c@camdenmecklem.com>
2023-03-13 12:10:21 -0400

    use torontomu banner for ryerson

--- ryerson_fake_data_gen.rb
+++
@@ -1,9 +0,0 @@
-x = 0
-while x < 100 do
-  User.create(
-    firstname: 'Testy',
-    lastname: 'Mc' + x.to_s + 'face',
-    email: ((0...8).map { (65 + rand(26)).chr }.join) + '@ryerson.edu',
-    agreement: true,
-    password: 'very secure'
-end
