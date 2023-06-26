8b3f632ced54375d317f9e67108ab6097624f8fd
Camden Mecklem <c@camdenmecklem.com>
2023-06-26 12:13:05 -0400

    make query more efficient

--- app/helpers/anthologies_helper.rb
+++ app/helpers/anthologies_helper.rb
@@ -1,2 +1,26 @@
 module AnthologiesHelper
+  def get_folders(anthology)
+    tags = ActsAsTaggableOn::Tagging.where(
+      tagger_id: anthology.id,
+      taggable_type: 'Document',
+      tagger_type: 'Anthology',
+      context: 'rep_folder'
+    ).joins(:tag).select('taggings.*, tags.*')
+
+    folders = []
+
+    tags.each do |t|
+      existing = folders.find { |f| f[:name] == t.name }
+      if existing
+        existing[:count] += 1
+      else
+        folders.push({
+          name: t.name,
+          count: 1
+        })
+      end
+    end
+
+    folders
+  end
 end
