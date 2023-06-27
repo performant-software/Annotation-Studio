2dc1f9071477fa1b23345cd68a076ffbdf16f2c9
Camden Mecklem <c@camdenmecklem.com>
2023-06-27 12:50:30 -0400

    Use alternative method for counting documents in folders (#467)
    * use alternative method for tag counts
    
    * make query more efficient
    
    * add comment and limit join scope
    
    * limit scope even more

--- app/helpers/anthologies_helper.rb
+++ app/helpers/anthologies_helper.rb
@@ -1,2 +1,30 @@
 module AnthologiesHelper
+
+  # Generate a list that contains the name
+  # of each folder in the given anthology  
+  # and the number of documents within it.
+  def get_folders(anthology)
+    tags = ActsAsTaggableOn::Tagging.where(
+      tagger_id: anthology.id,
+      taggable_type: 'Document',
+      tagger_type: 'Anthology',
+      context: 'rep_folder'
+    ).joins(:tag).select('tags.name')
+
+    folders = []
+
+    tags.each do |t|
+      existing = folders.find { |f| f[:name] == t.name }
+      if existing
+        existing[:count] += 1
+      else
+        folders.push({
+          name: t.name,
+          count: 1
+        })
+      end
+    end
+
+    folders
+  end
 end
