aa17b29cd9d7fb2703bc96ae81d6284e6c2cd0eb
Camden Mecklem <c@camdenmecklem.com>
2023-07-06 11:12:31 -0400

    cleanup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -69,7 +69,6 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
-  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => ENV['EMAIL_DOMAIN'] }
   config.action_mailer.postmark_settings = { :api_token => ENV['POSTMARK_TOKEN'] }
   config.action_mailer.perform_deliveries = true
