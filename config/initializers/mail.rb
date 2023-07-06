7d05213f69b45bc2613782ef317ec387508ad82c
Camden Mecklem <c@camdenmecklem.com>
2023-07-06 11:16:41 -0400

    remove all references to sendgrid

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,11 +1,3 @@
 if ['production', 'staging', 'public'].include?(Rails.env)
-  ActionMailer::Base.smtp_settings = {
-   :address        => 'smtp.sendgrid.net',
-   :port           => '587',
-   :authentication => :plain,
-   :user_name      => ENV['SENDGRID_USERNAME'],
-   :password       => ENV['SENDGRID_PASSWORD'],
-   :domain         => 'heroku.com'
-  }
-  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.delivery_method = :postmark
 end
